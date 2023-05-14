d223e280b90f283de7a1aedd254705dce1b00fbe
eliseusbrito <eliseusbrito@gmail.com>
2023-05-14 08:34:20 -0300

    Adicionado Consumer

---
+++ kafkaComJavaExpertosTech/kafka-consumer/src/main/java/br/com/eliseu/kafka/consumer/AplicacaoConsumer.java
@@ -0,0 +1,20 @@
+package br.com.eliseu.kafka.consumer;
+
+import br.com.eliseu.kafka.consumer.eventos.ConsumidorEvento;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AplicacaoConsumer {
+
+    public static void main(String[] args) {
+        AplicacaoConsumer aplicacao = new AplicacaoConsumer();
+        aplicacao.iniciar();
+    }
+
+    private void iniciar() {
+        log.info("Iniciando a aplicação Consumer");
+        ConsumidorEvento consumidor = new ConsumidorEvento();
+        consumidor.executar();
+    }
+
+}
